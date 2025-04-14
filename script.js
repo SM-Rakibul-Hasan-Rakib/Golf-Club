9c517b98e98557f14c56b53cc88ad7cffcea8b70
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2025-04-14 12:44:36 +0600

    page-1 animation done

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 gsap.to("#nav", {
   backgroundColor: "#000",
   duration: 0.5,
-  height: "130px",
+  height: "110px",
   // delay: 1,
   scrollTrigger: {
     trigger: "#nav",
     scroller: "body",
+    markers: true,
+    start: "top -10%",
+    end: "top -10%",
+    scrub: 2,
+  },
+});
+
+gsap.to("#main", {
+  backgroundColor: "#000",
+  scrollTrigger: {
+    trigger: "body",
+    markers: true,
+    start: "top -25vh",
+    end: "top -70%",
+    scrub: 2,
   },
 });
