11b2e3508ebc9dc9ed28dbbb8ad1982ee63da2eb
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2025-04-20 20:58:36 +0600

    day to start

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var csr = document.querySelector("#cursor");
 var blur = document.querySelector("#cursor-blur");
+
 document.addEventListener("mousemove", function (dets) {
   csr.style.left = dets.x + "px";
   csr.style.top = dets.y + "px";
-  blur.style.left = dets.x - 200 + "px";
-  blur.style.top = dets.y - 200 + "px";
+  blur.style.left = dets.x - 250 + "px";
+  blur.style.top = dets.y - 250 + "px";
 });
 
 gsap.to("#nav", {
@@ -30,5 +31,33 @@ gsap.to("#main", {
     start: "top -25vh",
     end: "top -70%",
     scrub: 2,
+  },
+});
+
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    csr.style.scale = 3;
+    csr.style.border = "1px solid #fff";
+    csr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    csr.style.scale = 1;
+    csr.style.border = "0px solid #95c11e";
+    csr.style.backgroundColor = "transparent";
+  });
+});
+gsap.from("#about-us img,#about-us-in", {
+  y: 50,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.4,
+  scrollTrigger: {
+    trigger: "#about-us",
+    scroller: "body",
+    markers: true,
+    start: "top 60%",
+    end: "top 58%",
+    scrub: 3,
   },
 });
